dcec077ba74e57e0112da62ced31af7a95c50f49
TiesP <pavel.o.71@gmail.com>
2016-04-08 20:47:52 +0200

    функция js сделана более грамотно

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-function run() {
-  var search = document.querySelectorAll('.search-hotel__button');
-  if(!search[0].classList.contains('search-hotel__button--in-progress')) {
-    search[0].classList.add('search-hotel__button--in-progress');
-  }
+function search(btn) {
+    if(!btn.classList.contains('search-hotel__button--in-progress')) {
+        btn.classList.add('search-hotel__button--in-progress');
+    } else {
+        btn.classList.remove('search-hotel__button--in-progress');
+    }
 }
